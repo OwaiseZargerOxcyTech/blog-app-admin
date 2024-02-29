c67f79a6bb81676cb2c3ff0e1d2a64c8e4c91889
owaisezarger <owaisezarger@gmail.com>
2024-02-29 11:27:20 +0530

    no-store cache-control

--- src/app/api/get-all-tutorial-subtopics/route.js
+++ src/app/api/get-all-tutorial-subtopics/route.js
@@ -6,7 +6,9 @@ const prisma = new PrismaClient();
 export async function GET(res) {
   try {
     const allTutorialSubtopics = await prisma.tutorialSubtopic.findMany();
-    return NextResponse.json(allTutorialSubtopics);
+    const response = NextResponse.json(allTutorialSubtopics);
+    response.headers.set("Cache-Control", "no-store");
+    return response;
   } catch (err) {
     console.error("Error fetching all tutorial subtopics:", err);
     return NextResponse.error("Failed to fetch tutorial subtopics", {
